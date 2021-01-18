383a1d832cbb0fee63d14e62ee7b4046e9613c49
Tobias Zaenker <tzaenker@informatik.uni-bonn.de>
2021-01-18 13:48:07 +0100

    Renamed module, updated compile script

--- compile_p3d_modules.sh
+++ compile_p3d_modules.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-if [ ! P3DModuleBuilder/.git ]; then
-  git clone https://github.com/Eruvae/P3DModuleBuilder
-fi
+#if [ ! P3DModuleBuilder/.git ]; then
+#  git clone https://github.com/Eruvae/P3DModuleBuilder
+#fi
+git submodule update --init
 
 cd P3DModuleBuilder
 git fetch
 git checkout p3d_voxgrid
 python3 build.py --clean
 cd ..
+
+cd pybind11_modules
+mkdir -p build
+cd build
+cmake ..
+make
+cp *.so ../..
