251a0e4930fac1f51acceef71b0e04d232e2f9e3
zeng <123Zxy>
2021-06-22 20:39:54 +0200

    test

--- train_agent_3d_dqn3_test.py
+++ train_agent_3d_dqn3_test.py
@@ -42,7 +42,7 @@ params = {
     'is_priority_buffer': True,
 
     # train params
-    'is_train': False,
+    'is_train': True,
     'visualise': False,
     'is_normalize': False,
     'num_episodes': 20,
