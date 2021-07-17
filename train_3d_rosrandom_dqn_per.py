8659771f7123df526cd4358a6576a94a572fd37f
zeng <123Zxy>
2021-07-18 00:58:57 +0200

    p3d_dqn_per_multiplier
    Former-commit-id: ec87210c4597849c9d1145e63c9fe35a7d657cf4

--- train_3d_rosrandom_dqn_per.py
+++ train_3d_rosrandom_dqn_per.py
@@ -5,7 +5,7 @@ import os
 import agents
 import network
 import field_ros
-
+import action_space
 from config.config_dqn import ConfigDQN
 from train.RosRandomeTrainer import RosRandomTrainer
 from utilities.data_structures.Constant import EpsExplorationStrategy
@@ -15,6 +15,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), os.path.pardir))
 network = network.network_dqn.DQN_Network11
 Agent = agents.DQN_agents.DDQN_With_Prioritised_Experience_Replay.DDQN_With_Prioritised_Experience_Replay
 Field = field_ros.Field
+Action = action_space.ActionMoRo12
 
 out_folder = "output_reset_and_random6"
 in_folder = "output_reset_and_random5"
@@ -36,6 +37,6 @@ config = ConfigDQN(network=network,
                    eps_exploration_strategy_config=eps_exploration_strategy_config
                    )
 
-trainer = RosRandomTrainer(config=config, Agent=Agent, Field=Field)
+trainer = RosRandomTrainer(config=config, Agent=Agent, Field=Field, Action=Action)
 trainer.agent.load_model(index=36)
 trainer.train()
