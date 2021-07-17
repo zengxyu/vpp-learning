c4e281f9e6351e7a9bcf9f5c8d4c96b4dba2d0c4
zeng <123Zxy>
2021-07-17 23:33:21 +0200

    fix some bugs
    Former-commit-id: bab84ffa85637f2f367c1b5169b6d64d7c0d380f

--- agents/DQN_agents/DQN_With_Fixed_Q_Targets.py
+++ agents/DQN_agents/DQN_With_Fixed_Q_Targets.py
@@ -2,6 +2,7 @@ import copy
 
 from agents.Base_Agent_DQN import Base_Agent_DQN
 from agents.DQN_agents.DQN import DQN
+from collections import Counter
 
 
 class DQN_With_Fixed_Q_Targets(DQN):
@@ -13,6 +14,25 @@ class DQN_With_Fixed_Q_Targets(DQN):
         self.q_network_target = self.create_NN()
         Base_Agent_DQN.copy_model_over(from_model=self.q_network_local, to_model=self.q_network_target)
         self.model_dict, self.optimizer_dict = self.__build_model_and_optimizer_dict()
+
+    def learn(self, experiences=None):
+        """Runs a learning iteration for the Q network"""
+        if experiences is None:
+            states, actions, rewards, next_states, dones = self.memory.sample()  # Sample experiences
+        else:
+            states, actions, rewards, next_states, dones = experiences
+        loss = self.compute_loss(states, next_states, rewards, actions, dones)
+
+        actions_list = [action_X.item() for action_X in actions]
+
+        self.logger.info("Action counts {}".format(Counter(actions_list)))
+        self.take_optimisation_step(self.q_network_optimizer, self.q_network_local, loss,
+                                    self.hyper_parameters["gradient_clipping_norm"])
+        self.skipping_step_update_of_target_network(self.q_network_local, self.q_network_target,
+                                                    global_step_number=self.global_step_number,
+                                                    update_every_n_steps=self.hyper_parameters[
+                                                        "update_every_n_steps"])
+        return loss.detach().cpu().numpy()
 
     def compute_q_values_for_next_states(self, next_states):
         """Computes the q_values for next state we will use to create the loss to train the Q network"""
