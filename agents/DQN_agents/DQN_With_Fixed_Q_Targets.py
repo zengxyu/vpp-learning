7f48eab7236a57fb1f0ef35c4ccb50c866e0ceb6
zeng <123Zxy>
2021-07-17 14:55:24 +0200

    fix some bugs
    Former-commit-id: c5e88b89120adaaecc3fb75167ae992da13e8f1f

--- agents/DQN_agents/DQN_With_Fixed_Q_Targets.py
+++ agents/DQN_agents/DQN_With_Fixed_Q_Targets.py
@@ -14,13 +14,6 @@ class DQN_With_Fixed_Q_Targets(DQN):
         Base_Agent_DQN.copy_model_over(from_model=self.q_network_local, to_model=self.q_network_target)
         self.model_dict, self.optimizer_dict = self.__build_model_and_optimizer_dict()
 
-    def learn(self, experiences=None):
-        """Runs a learning iteration for the Q network"""
-        loss = super(DQN_With_Fixed_Q_Targets, self).learn(experiences=experiences)
-        self.soft_update_of_target_network(self.q_network_local, self.q_network_target,
-                                           self.hyper_parameters["tau"])  # Update the target network
-        return loss
-
     def compute_q_values_for_next_states(self, next_states):
         """Computes the q_values for next state we will use to create the loss to train the Q network"""
         Q_targets_next = self.q_network_target(next_states).detach().max(1)[0].unsqueeze(1)
