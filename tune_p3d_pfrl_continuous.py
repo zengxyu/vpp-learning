ee818b755bdb3633f20c5539745bf7ce0348d44f
zeng <123Zxy>
2021-08-03 21:08:15 +0200

    train rainbow
    Former-commit-id: e8c36fe025b4ba618ca046615b7379c8ddfbbdf1

--- tune_p3d_pfrl_continuous.py
+++ tune_p3d_pfrl_continuous.py
@@ -9,7 +9,7 @@ import action_space
 from agent_pfrl.agent_type import AgentType
 from environment import field_p3d_discrete, field_p3d_continuous
 
-from config.config_dqn import ConfigDQN
+from config.config_ac import ConfigAC
 from train_pfrl.P3DTrainer_PFRL import P3DTrainer_PFRL
 from utilities.util import get_project_path
 
@@ -44,22 +44,19 @@ def build_sac():
 
 
 def train_fun(tuning_param):
-    if tuning_param['alg'] == AgentType.Agent_Rainbow:
-        Field, Action, out_folder, in_folder = build_rainbow()
-    elif tuning_param['alg'] == AgentType.Agent_DDQN_PER:
-        Field, Action, out_folder, in_folder = build_ddqn_per()
-    elif tuning_param['alg'] == AgentType.Agent_SAC:
+    if tuning_param['alg'] == AgentType.Agent_SAC:
         Field, Action, out_folder, in_folder = build_sac()
     else:
         Field, Action, out_folder, in_folder = None, None, None, None
     # network
-    config = ConfigDQN(network=None,
-                       out_folder=out_folder,
-                       in_folder=in_folder,
-                       learn_every=1,
-                       console_logging_level=logging.DEBUG,
-                       file_logging_level=logging.WARNING,
-                       )
+    config = ConfigAC(actor_network=None,
+                      critic_network=None,
+                      out_folder=out_folder,
+                      in_folder=in_folder,
+                      learn_every=1,
+                      console_logging_level=logging.DEBUG,
+                      file_logging_level=logging.WARNING,
+                      )
     config.set_parameters(tuning_param)
     trainer = P3DTrainer_PFRL(config=config, agent_type=tuning_param['alg'], Field=Field, Action=Action,
                               project_path=tuning_param["project_path"])
@@ -75,11 +72,10 @@ if __name__ == '__main__':
         config={
             "learning_rate": tune.grid_search([1e-3, 1e-4]),
             "discount_rate": tune.grid_search([0.9, 0.95, 0.98]),
-            "epsilon_decay_rate": tune.grid_search([0.99, 0.99975, 0.9999999]),
             # "learning_rate": tune.grid_search([1e-4]),
             # "discount_rate": tune.grid_search([0.98]),
             "project_path": project_path,
-            "alg": tune.grid_search([AgentType.Agent_Rainbow])
+            "alg": tune.grid_search([AgentType.Agent_SAC])
         },
         log_to_file=True,
         resources_per_trial={'cpu': 1, 'gpu': 0}
