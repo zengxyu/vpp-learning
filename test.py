7b30bdaa1921e59c2d95b29b006f217cd763f8e1
zeng <123Zxy>
2021-08-07 21:52:58 +0200

    nan error
    Former-commit-id: 40859f5f8bff281f11e62b3bc6b750912eaea046

--- test.py
+++ test.py
@@ -5,4 +5,12 @@ print(np.e ** -0.5)
 print(np.e ** -0.25)
 array = np.array([1, 2, 3])
 b = np.array([3, 2, 2])
-print(array/b)
+print(array / b)
+
+
+def nan_to_num(n):
+    NEAR_0 = 1e-15
+    return np.clip(n, NEAR_0, 1 - NEAR_0)
+
+
+print(nan_to_num(np.NaN))
