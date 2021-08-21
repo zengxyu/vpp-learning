c7528c0a666fd6c59e1a1275374c3874daac588d
zeng <123Zxy>
2021-08-21 18:46:44 +0200

    known global map
    Former-commit-id: 518869510d1b39ee087f89431e67f807b4cd0d51

--- network/__init__.py
+++ network/__init__.py
@@ -2,3 +2,4 @@ from network import network_dqn
 from network import network_ac_discrete
 from network import network_ac_continuous
 from network import network_dqn_vae
+from network import network_known_map
--- test/test.py
+++ test/test.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class Parent(object):
     def __init__(self):
         print("Parent init")
@@ -23,11 +26,14 @@ class Children(Parent):
 
 
 if __name__ == '__main__':
-    child = Children()
-    child.say()
-import torch
-
-a = torch.rand(4, 3, 28, 28)
-ind = torch.tensor([0, 2])
-c = a.index_select(0, ind)
-print(c.shape)
+    arr = np.array([0, 0, 0, 1, 1])
+    print(np.sum(arr == 1))
+# if __name__ == '__main__':
+#     child = Children()
+#     child.say()
+# import torch
+#
+# a = torch.rand(4, 3, 28, 28)
+# ind = torch.tensor([0, 2])
+# c = a.index_select(0, ind)
+# print(c.shape)
