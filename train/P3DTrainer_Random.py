7b18af5d6fd0c724c7c80b5d903b42bb414aeedd
zeng <123Zxy>
2021-08-29 00:05:29 +0200

    known_map, lstm, weighted reward, random env
    Former-commit-id: 9a8882b7d76d75546f88bf6a0cc860c1b0ba721c

--- train/P3DTrainer_Random.py
+++ train/P3DTrainer_Random.py
@@ -24,8 +24,6 @@ class P3DTrainer(object):
         self.agent = agent
         self.field = field
         self.seq_len = 5
-        self.deque = Pose_State_DEQUE(capacity=self.seq_len)
-        self.max_steps = self.field.max_steps
 
     def train(self, is_sph_pos, is_global_known_map, is_egocetric, is_randomize,
               is_reward_plus_unknown_cells, randomize_control):
@@ -52,7 +50,6 @@ class P3DTrainer(object):
             rewards = []
             found_targets = []
             actions = []
-            step = 0
             self.agent.reset()
 
             observed_map, robot_pose = self.field.reset(is_sph_pos=is_sph_pos,
@@ -66,53 +63,24 @@ class P3DTrainer(object):
             while not done:
                 loss = 0
 
-                # robot direction
-                robot_direction = Rotation.from_quat(robot_pose[3:]).as_matrix() @ initial_direction
-                robot_pose_input = np.concatenate([robot_pose[:3], robot_direction.squeeze()], axis=0)
-                self.deque.append(robot_pose_input)
-
-                # 前5步，随便选择一个动作
-                if step <= self.seq_len:
-                    action = random.randint(0, self.field.get_action_size())
-                else:
-                    action = self.agent.pick_action([observed_map, self.deque.get_robot_poses()])
+                action = random.randint(0, 107)
                 (observed_map_next, robot_pose_next), found_target_num, unknown_cells_num, done, _ = self.field.step(
                     action)
                 if is_reward_plus_unknown_cells:
-                    reward = found_target_num + 0.005 * unknown_cells_num ** (1 - step / self.max_steps)
+                    reward = found_target_num + 0.001 * unknown_cells_num
                 else:
                     reward = found_target_num
-                # if robot_pose is the same with the robot_pose_next, then reward--
-                robot_direction_next = Rotation.from_quat(robot_pose_next[3:]).as_matrix() @ initial_direction
-
-                # diff direction
-                robot_pose_input_next = np.concatenate([robot_pose_next[:3], robot_direction_next.squeeze()], axis=0)
-
-                self.deque.append_next(robot_pose_input_next)
-
-                self.agent.step(state=[observed_map, robot_pose_input], action=action, reward=reward,
-                                next_state=[observed_map_next, robot_pose_input_next], done=done)
-
-                # to the next state
-                observed_map = observed_map_next.copy()
-                robot_pose = robot_pose_next.copy()
-                # train
-                if time_step % self.config.learn_every == 0 and time_step > self.seq_len:
-                    loss = self.agent.learn()
 
                 actions.append(action)
                 rewards.append(reward)
                 found_targets.append(found_target_num)
                 losses.append(loss)
                 time_step += 1
-                step += 1
                 # record
                 if not headless:
                     threading.Thread.considerYield()
 
                 if done:
-                    step = 0
-                    self.deque.clear()
                     last_targets_found = np.sum(found_targets)
                     print("\nepisode {} over".format(i_episode))
                     print("robot pose: {}".format(robot_pose[:3]))
--- train/__init__.py
+++ train/__init__.py
@@ -1,3 +1,4 @@
 from train import P3DTrainer_Temporal2
 from train import P3DTrainer
 from train import P3DTrainer_Temporal_Pose
+from train import P3DTrainer_Random
