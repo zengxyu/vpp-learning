e4f9ff1e0933c908ff04e4cc4cd1116a27dd0a0e
zeng <123Zxy>
2021-08-29 11:23:16 +0200

    known_map, lstm, weighted reward, random env
    Former-commit-id: ac178fc427ddfc0573fb93f87a82200f0f5e2d7b

--- train/RosRandomeTrainer_Temporal_Pose.py
+++ train/RosRandomeTrainer_Temporal_Pose.py
@@ -1,5 +1,3 @@
-import logging
-import os
 import random
 
 import numpy as np
@@ -10,12 +8,8 @@ from train.StateDeque import Pose_State_DEQUE
 from utilities.basic_logger import BasicLogger
 from utilities.summary_writer import SummaryWriterLogger
 
-headless = True
-if not headless:
-    from direct.stdpy import threading
 
-
-class P3DTrainer(object):
+class RosRandomTrainerTemporalPose(object):
     def __init__(self, config, agent, field):
         self.config = config
         self.summary_writer = SummaryWriterLogger(config)
@@ -29,32 +23,22 @@ class P3DTrainer(object):
 
     def train(self, is_sph_pos, is_global_known_map, is_egocetric, is_randomize,
               is_reward_plus_unknown_cells, randomize_control):
-        if headless:
-            self.main_loop(is_sph_pos, is_global_known_map, is_egocetric, is_randomize, is_reward_plus_unknown_cells,
-                           randomize_control)
-        else:
-            # field.gui.taskMgr.setupTaskChain('mainTaskChain', numThreads=1)
-            # field.gui.taskMgr.add(main_loop, 'mainTask', taskChain='mainTaskChain')
-            main_thread = threading.Thread(target=self.main_loop)
-            main_thread.start()
-            self.field.gui.run()
-
-    def main_loop(self, is_sph_pos, is_global_known_map, is_egocetric, is_randomize, is_reward_plus_unknown_cells,
-                  randomize_control):
         time_step = 0
         initial_direction = np.array([[1], [0], [0]])
         last_targets_found = 0
+
         for i_episode in range(self.config.num_episodes_to_run):
             print("\nepisode {}".format(i_episode))
+            step_count = 0
             e_start_time = time.time()
             done = False
-            losses = []
             rewards = []
             found_targets = []
             unknown_cells = []
             known_cells = []
             actions = []
-            step = 0
+            losses = []
+            zero_reward_consistent_count = 0
             self.agent.reset()
 
             observed_map, robot_pose = self.field.reset(is_sph_pos=is_sph_pos,
@@ -63,42 +47,34 @@ class P3DTrainer(object):
                                                         is_randomize=is_randomize,
                                                         randomize_control=randomize_control,
                                                         last_targets_found=last_targets_found)
-            print("robot pose:{}".format(robot_pose))
-            print("observation size:{}; robot pose size:{}".format(observed_map.shape, robot_pose.shape))
             while not done:
-                loss = 0
-
                 # robot direction
                 robot_direction = Rotation.from_quat(robot_pose[3:]).as_matrix() @ initial_direction
                 robot_pose_input = np.concatenate([robot_pose[:3], robot_direction.squeeze()], axis=0)
                 self.deque.append(robot_pose_input)
 
-                # 前5步，随便选择一个动作
-                if step <= self.seq_len:
+                if step_count <= self.seq_len:
                     action = random.randint(0, self.field.get_action_size() - 1)
                 else:
                     action = self.agent.pick_action([observed_map, self.deque.get_robot_poses()])
-                (observed_map_next,
-                 robot_pose_next), found_target_num, unknown_cells_num, known_cells_num, done, _ = self.field.step(
-                    action)
-                if is_reward_plus_unknown_cells:
-                    # a = found_target_num
-                    # b = 0.05 * unknown_cells_num ** (
-                    #         1 - step / self.max_steps / 2)
-                    # c = - (known_cells_num / 2000) ** 1.1
-                    reward = found_target_num + 0.008 * unknown_cells_num
-                    if reward < 0:
-                        reward = 0
-                else:
-                    reward = found_target_num
+
+                (observed_map_next, robot_pose_next), found_target_num, done, _ = self.field.step(action)
+                reward = found_target_num
+
                 # if robot_pose is the same with the robot_pose_next, then reward--
+                if reward == 0:
+                    zero_reward_consistent_count += 1
+                else:
+                    zero_reward_consistent_count = 0
+                # # reward redefine
+                if zero_reward_consistent_count >= 10:
+                    done = True
                 robot_direction_next = Rotation.from_quat(robot_pose_next[3:]).as_matrix() @ initial_direction
 
                 # diff direction
                 robot_pose_input_next = np.concatenate([robot_pose_next[:3], robot_direction_next.squeeze()], axis=0)
 
                 self.deque.append_next(robot_pose_input_next)
-
                 self.agent.step(state=[observed_map, robot_pose_input], action=action, reward=reward,
                                 next_state=[observed_map_next, robot_pose_input_next], done=done)
 
@@ -106,52 +82,40 @@ class P3DTrainer(object):
                 observed_map = observed_map_next.copy()
                 robot_pose = robot_pose_next.copy()
                 # train
-                if time_step % self.config.learn_every == 0 and time_step > self.seq_len:
+                if self.config.is_train and time_step > self.seq_len:
                     loss = self.agent.learn()
+                else:
+                    loss = 0
+                time_step += 1
+                step_count += 1
+
+                print(
+                    "{}-th episode : {}-th step takes {} secs; action:{};reward:{}; sum reward:{}".format(
+                        i_episode, step_count, time.time() - e_start_time, action, reward, np.sum(rewards) + reward))
+                # record
 
                 actions.append(action)
                 rewards.append(reward)
                 found_targets.append(found_target_num)
-                unknown_cells.append(unknown_cells_num)
-                known_cells.append(known_cells_num)
                 losses.append(loss)
-                time_step += 1
-                step += 1
-                # record
-                if not headless:
-                    threading.Thread.considerYield()
-
                 if done:
-                    step = 0
+                    step_count = 0
                     self.deque.clear()
                     last_targets_found = np.sum(found_targets)
                     print("\nepisode {} over".format(i_episode))
+                    print("mean rewards1:{}".format(np.sum(rewards)))
                     print("robot pose: {}".format(robot_pose[:3]))
                     print("actions:{}".format(np.array(actions)))
                     print("rewards:{}".format(np.array(rewards)))
-                    print("found_targets:{}".format(np.array(found_targets)))
-
-                    print(
-                        "Episode : {} | Mean loss : {} | Reward : {} | Found_targets : {} | unknown_cells :{} | known cells :{}".format(
-                            i_episode,
-                            np.mean(losses),
-                            np.sum(rewards),
-                            np.sum(
-                                found_targets), np.sum(unknown_cells), np.sum(known_cells)))
-
                     mean_loss_last_n_ep, mean_reward_last_n_ep = self.summary_writer.update(np.mean(losses),
-                                                                                            np.sum(found_targets),
-                                                                                            i_episode, verbose=False)
+                                                                                            np.sum(rewards),
+                                                                                            i_episode)
+                    if np.sum(rewards) == 0:
+                        self.field.reset_stuck_env()
 
-                    if (i_episode + 1) % self.config.save_model_every == 0:
+                    if (i_episode + 1) % 3 == 0:
                         self.agent.store_model()
 
                     e_end_time = time.time()
                     print("episode {} spent {} secs".format(i_episode, e_end_time - e_start_time))
         print('Complete')
-
-    def get_state_size(self, field):
-        return 0
-
-    def get_action_size(self, field):
-        return field.get_action_size()
---
+++ train_ros_random_discrete_temporal2_postion_lstm.py
@@ -0,0 +1,55 @@
+import argparse
+import logging
+import sys
+import os
+
+import action_space
+import agents
+import network
+import train
+import environment
+from config.config_dqn import ConfigDQN
+from utilities.util import get_project_path, get_state_size, get_action_size
+
+sys.path.append(os.path.join(os.path.dirname(__file__), os.path.pardir))
+
+
+def train_fun():
+    Network = network.network_dqn_11_temporal.DQN_Network11_Temporal_LSTM3
+    Agent = agents.DQN_agents.Agent_DDQN_PER_Temporal_Pose.Agent_DDQN_PER_Time_KnownMap
+    Field = environment.field_ros.Field
+    Action = action_space.ActionMoRoMul108
+    Trainer = train.RosRandomeTrainer_Temporal_Pose.RosRandomTrainerTemporalPose
+    out_folder = "out_ros_random_temporal_pose_random_108_control2"
+    in_folder = ""
+    # network
+    config = ConfigDQN(network=Network,
+                       out_folder=out_folder,
+                       in_folder=in_folder,
+                       learn_every=1,
+                       console_logging_level=logging.DEBUG,
+                       file_logging_level=logging.WARNING,
+                       )
+
+    # field
+    field = Field(config=config, Action=Action, shape=(256, 256, 256), sensor_range=50, hfov=90.0, vfov=60.0,
+                  max_steps=400, handle_simulation=True)
+    config.set_parameters({"learning_rate": 3e-5})
+    # Agent
+    agent = Agent(config)
+
+    trainer = Trainer(config=config, agent=agent, field=field)
+    trainer.train(is_sph_pos=False, is_randomize=True, is_global_known_map=False, is_egocetric=False,
+                  is_reward_plus_unknown_cells=True,
+                  randomize_control=True)
+
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--info', metavar='N', type=str, nargs='+',
+                    help='an integer for the accumulator')
+
+args = parser.parse_args()
+if __name__ == '__main__':
+    project_path = get_project_path()
+
+    train_fun()
