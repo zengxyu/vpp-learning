45f8c48310c893c939766285e334ec8a92d17d8e
zeng <123Zxy>
2021-09-03 00:42:32 +0200

    remove files
    Former-commit-id: b7506a48a26227c0c32077ace83c7f3730b412be

--- train_p3d_static_spacial_lstm.py
+++ train_p3d_static_spacial_lstm.py
@@ -24,7 +24,7 @@ def train_fun():
     Field = environment.field_p3d_discrete.Field
     Action = action_space.ActionMoRoMultiplier36
     Trainer = trainer_p3d.P3DTrainer_Temporal.P3DTrainer
-    out_folder = "out_p3d_static_env_seq_len_10_spacial"
+    out_folder = "p3d_static_env_seq_len_10_spacial_lstm_action_36"
     in_folder = ""
     # network
     config = ConfigDQN(network=Network,
