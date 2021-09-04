aed9738d5f261483f37d7148863b4fd964fcb82f
zeng <123Zxy>
2021-09-04 23:30:54 +0200

    remove files
    Former-commit-id: 115c2e5bb51b87436076fea7db13b81158e7a6b4

--- trainer_p3d/P3DTrainer_relative_pose.py
+++ trainer_p3d/P3DTrainer_relative_pose.py
@@ -144,8 +144,7 @@ class P3DTrainer(object):
                 relative_pose = relative_pose_next.copy()
                 # trainer_p3d
                 if self.global_time_step % self.config.learn_every == 0 and self.config.is_train:
-                    for i in range(12):
-                        loss = self.agent.learn()
+                    loss = self.agent.learn()
 
                 actions.append(action)
                 rewards.append(reward)
