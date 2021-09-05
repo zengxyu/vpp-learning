2d92e67665bed70325c2c0233abb739c630fb70a
zeng <123Zxy>
2021-09-05 02:32:57 +0200

    remove files
    Former-commit-id: cb5da867d0cd321eeab8b1fa91df5970fe20504a

--- network/network_dqn_11_spacial.py
+++ network/network_dqn_11_spacial.py
@@ -135,6 +135,102 @@ class DQN_Network11_Temporal_Spacial1(torch.nn.Module):
         return val
 
 
+class DQN_Network11_Temporal_Spacial1_3(torch.nn.Module):
+    def __init__(self, action_size):
+        super().__init__()
+        # 3 5 4 2
+        # 15 36 18
+        self.frame_con1 = torch.nn.Conv2d(15, 24, kernel_size=4, stride=2, padding=1)
+        self.frame_fc1 = torch.nn.Linear(3888, 784)
+        self.frame_fc2 = torch.nn.Linear(784, 128)
+
+        self.frame_nb_fc1 = torch.nn.Linear(405, 128)
+        self.frame_nb_fc2 = torch.nn.Linear(128, 48)
+
+        self.hn_neighbor_state_dim = 192
+        self.lstm_neighbor = torch.nn.LSTM(48, self.hn_neighbor_state_dim, batch_first=True)
+        self.lstm_neighbor_fc = torch.nn.Linear(self.hn_neighbor_state_dim, 64)
+        # lstm neighbor output = 128
+
+        self.pose_fc1a = torch.nn.Linear(3, 32)
+        self.pose_fc2a = torch.nn.Linear(32, 64)
+
+        self.pose_fc1b = torch.nn.Linear(3, 32)
+        self.pose_fc2b = torch.nn.Linear(32, 64)
+
+        self.pose_fc3 = torch.nn.Linear(128 + 64, 128)
+
+        self.fc_val = torch.nn.Linear(128, action_size)
+
+    def init_weights(self):
+        for m in self.modules():
+            if type(m) is torch.nn.Linear or type(m) is torch.nn.Conv2d:
+                torch.nn.init.zeros_(m.weight)
+                torch.nn.init.zeros_(m.bias)
+
+    def encode_neighbor(self, frame):
+        # 15 36 18
+        batch_size, depth, w, h = frame.shape
+        frame = torch.reshape(frame, (batch_size, 3, 5, 36, 18))
+        known_frame = frame[:, 2, :, :, :]  # batch_size, 5, 36, 18
+        known_frame = torch.reshape(known_frame, (-1, 5, 4, 9, 2, 9))
+        known_frame = torch.transpose(known_frame, 1, 2)  # batch_size, 4, 5, 9, 2, 9
+        known_frame = torch.transpose(known_frame, 3, 4)  # batch_size, 4, 5, 2, 9, 9
+        known_frame = torch.transpose(known_frame, 2, 3)  # batch_size, 4, 2, 5, 9, 9
+        known_frame = torch.reshape(known_frame, (batch_size, 8, 405))
+        known_frame = torch.transpose(known_frame, 0, 1)  #
+        seq_len = known_frame.shape[0]  # 8
+        h0 = torch.zeros(1, batch_size, self.hn_neighbor_state_dim).to(
+            torch.device("cpu"))
+        c0 = torch.zeros(1, batch_size, self.hn_neighbor_state_dim).to(
+            torch.device("cpu"))
+        outs = None
+        for i in range(seq_len):
+            out = self.encode_region(known_frame[i])
+            out = out.unsqueeze(1)
+            if outs is None:
+                outs = out
+            else:
+                outs = torch.cat((outs, out), dim=1)
+        lstm_neighbor_output, (hn, cn) = self.lstm_neighbor(outs, (h0, c0))
+        hn_neighbor = hn.squeeze(0)
+        hn_neighbor = F.relu(self.lstm_neighbor_fc(hn_neighbor))
+        return hn_neighbor
+
+    def encode_region(self, region_frame):
+        # out_frame = F.relu(self.frame_nb_con1(region_frame))
+        # out_frame = F.relu(self.frame_nb_con2(out_frame))
+
+        # out_frame = region_frame.reshape(out_frame.size()[0], -1)
+        out_frame = F.relu(self.frame_nb_fc1(region_frame))
+        out_frame = F.relu(self.frame_nb_fc2(out_frame))
+
+        return out_frame
+
+    def encode_observation_map(self, frame):
+        out_frame = F.relu(self.frame_con1(frame))
+        # out_frame = F.relu(self.frame_con2(out_frame))
+        out_frame = out_frame.reshape(out_frame.size()[0], -1)
+        out_frame = F.relu(self.frame_fc1(out_frame))
+        out_frame = F.relu(self.frame_fc2(out_frame))
+
+        return out_frame
+
+    def forward(self, state):
+        frame, robot_pose = state
+        batch_size = robot_pose.shape[0]
+        seq_len = robot_pose.shape[1]
+        robot_pose_reshape = torch.transpose(robot_pose, 0, 1)
+        out_frame = self.encode_observation_map(frame)
+        frame_spacial = self.encode_neighbor(frame)
+        outs = torch.cat((out_frame, frame_spacial), dim=1)
+
+        out = F.relu(self.pose_fc3(outs))
+
+        val = self.fc_val(out)
+        return val
+
+
 class DQN_Network11_Temporal_Spacial1_2(torch.nn.Module):
     """spacial lstm不管深度，只管方向"""
 
@@ -318,7 +414,8 @@ class DQN_Network11_Temporal_Spacial2(torch.nn.Module):
         frame = torch.transpose(frame, 0, 1)  # 8, bs, 15, 9, 9
         # 在8这个维度上，对batch中的每一个，求出target最多的
         known_frame = frame[:, :, 2, :]  # 8, bs, 405
-        regions_target_num = torch.sum(known_frame, dim=-1).unsqueeze(-1) # 8, bs
+        regions_target_num = torch.sum(known_frame, dim=-1).unsqueeze(-1)  # 8, bs
+        # 从大到小排列，free cells少的最好，排在后面
         index = torch.argsort(regions_target_num, dim=0, descending=False)  # 8, bs, 1
         index = index.repeat(1, 1, 1215)
         frame = torch.reshape(frame, shape=(8, -1, 1215))  # 8, bs, 1215
