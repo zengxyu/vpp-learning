2e6310805f98e5b8851c4e26c59d78ae191e6250
zeng <123Zxy>
2022-01-16 20:30:15 +0100

    change some relative path
    Former-commit-id: 8189e2abbe707cc988b90c3c72a6c2a180b41bae

--- scripts/run.py
+++ scripts/run.py
@@ -11,7 +11,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "autoencoder", "dataset"
 from ae_config import get_parse_args
 from autoencoder.utility import setup_logger
 from autoencoder.learner import AELearner
-from vpp_env_client import EnvironmentClient
+# from vpp_env_client import EnvironmentClient
 
 setup_logger()
 
@@ -24,7 +24,7 @@ if __name__ == "__main__":
         learner.training()
     elif config.phase == "eval":
         learner.evaluation()
-    else:
-        client = EnvironmentClient(handle_simulation=False)
-        voxelgrid, robotPose, robotJoints, reward = client.sendReset(map_type='voxelgrid')
-        learner.inference(voxelgrid)
+    # else:
+    #     client = EnvironmentClient(handle_simulation=False)
+    #     voxelgrid, robotPose, robotJoints, reward = client.sendReset(map_type='voxelgrid')
+    #     learner.inference(voxelgrid)
