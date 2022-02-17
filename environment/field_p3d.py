fix the bug: compute the rotation between two rotations. add world name, base variables to  env_ros.configuration
Former-commit-id: 686b0a5366d9c785a033dcdaf6780dceca1746b2

environment/field_p3d.py
@@ -5,11 +5,13 @@ import time
 
 import field_env_3d_helper
 from field_env_3d_helper import Vec3D
+from pyquaternion import Quaternion
 from scipy.spatial.transform import Rotation
 from environment.utilities.check_occupied_helper import has_obstacle, in_bound_boxes, out_of_world
 from environment.utilities.map_concat_helper import concat
 from environment.utilities.plant_models_loader import load_plants
 from environment.utilities.random_env_helper import get_random_multi_plant_models
+from environment.utilities.rotation_helper import get_rotation_between_rotations
 
 from utilities.util import get_project_path
 import numpy as np
@@ -193,8 +195,6 @@ class FieldP3D:
         self.visit_shape = (int(self.shape[0] // self.visit_resolution), int(self.shape[1] // self.visit_resolution),
                             int(self.shape[2] // self.visit_resolution))
         self.visit_map = np.zeros(shape=self.visit_shape, dtype=np.uint8)
-
-        self.calculate_observable_cells_in_total()
 
         print("#observable_roi/#roi = {}".format(self.observable_roi_total / self.roi_total))
         print("#observable_occ/#occ = {}".format(self.observable_occ_total / self.occ_total))

environment/field_ros.py
@@ -4,7 +4,8 @@ import numpy as np
 from scipy.spatial.transform import Rotation
 
 from environment.utilities.map_concat_helper import concat
-from scripts.vpp_env_client import EnvironmentClient
+from environment.utilities.rotation_helper import get_rotation_between_rotations
+from environment.scripts.vpp_env_client import EnvironmentClient
 
 
 class FieldRos:
@@ -18,6 +19,7 @@ class FieldRos:
 
         self.randomize = self.env_config["randomize"]
         self.handle_simulation = self.env_config["handle_simulation"]
+        self.resolution = self.env_config["resolution"]
 
         self.action_space = action_space
         self.reset_count = 0
@@ -43,7 +45,8 @@ class FieldRos:
         self.visit_map = None
         self.map = None
 
-        self.client = EnvironmentClient(handle_simulation=self.handle_simulation)
+        self.client = EnvironmentClient(self.handle_simulation, self.env_config["world_name"], self.env_config["base"],
+                                        self.parser_args.head)
         if self.handle_simulation:
             self.client.startSimulation()
 
@@ -67,6 +70,7 @@ class FieldRos:
         axes = self.robot_rot.as_matrix().transpose()
         relative_move, relative_rot = self.action_space.get_relative_move_rot(axes, action, self.MOVE_STEP,
                                                                               self.ROT_STEP)
+
         relative_pose = np.append(relative_move, relative_rot.as_quat()).tolist()
         unknown_map, known_free_map, known_occupied_map, known_roi_map, robot_pose, \
         found_roi, found_occ, found_free = self.client.sendRelativePose(relative_pose)
@@ -75,8 +79,7 @@ class FieldRos:
         robot_rot = Rotation.from_quat(np.array(robot_pose[3:]))
 
         self.relative_position = robot_pos - self.robot_pos
-        relative_rotation, _ = Rotation.align_vectors([robot_rot.as_rotvec()], [self.robot_rot.as_rotvec()])
-        self.relative_rotation = relative_rotation
+        self.relative_rotation = get_rotation_between_rotations(self.robot_rot, robot_rot)
 
         self.robot_pos = robot_pos
         self.robot_rot = robot_rot
@@ -95,6 +98,7 @@ class FieldRos:
 
         info = {"visit_gain": 0, "new_free_cells": found_free, "new_occupied_cells": found_occ,
                 "new_found_rois": found_roi, "reward": reward, "coverage_rate": np.nan}
+
         return inputs, reward, done, info
 
     def get_reward(self, visit_gain, found_free, found_occ, found_roi):
@@ -132,7 +136,8 @@ class FieldRos:
         # but the limitation from -1 to 1 was mainly for the static arm
         unknown_map, known_free_map, known_occupied_map, known_roi_map, robot_pose, new_roi_cells, new_occupied_cells, new_free_cells = self.client.sendReset(
             randomize=self.randomize, min_point=[-1, -1, -0.1], max_point=[1, 1, 0.1], min_dist=0.4)
-
+        self.robot_pos = np.array(robot_pose[:3])
+        self.robot_rot = Rotation.from_quat(np.array(robot_pose[3:]))
         self.map = concat(unknown_map, known_free_map, known_occupied_map, known_roi_map, np.uint8)
         inputs = self.get_inputs()
 

trainer/RosTrainer.py
@@ -22,21 +22,6 @@ from trainer.trainer_helper import save_episodes_info
 from utilities.info import EpisodeInfo
 
 
-def print_step_info(episode, step, info):
-    new_free_cells = info["new_free_cells"]
-    new_occupied_cells = info["new_occupied_cells"]
-    new_rois = info["new_found_rois"]
-    reward = info["reward"]
-    print(
-        "Episode : {}; step : {}; found free cells : {}; found occupied cells : {}; found rois : {}; reward:{}\n".format(
-            episode,
-            step,
-            new_free_cells,
-            new_occupied_cells,
-            new_rois,
-            reward))
-
-
 class RosTrainer(object):
     def __init__(self, env, agent, action_space, parser_args):
         self.parser_args = parser_args
@@ -51,6 +36,9 @@ class RosTrainer(object):
         self.global_i_step = 0
         self.train_collector = EpisodeInfo(self.training_config["train_smooth_n"])
         self.test_collector = EpisodeInfo(self.training_config["test_smooth_n"])
+        self.train_step_collector = {}
+        self.test_step_collector = {}
+
         # discrete
         if not parser_args.train or parser_args.resume:
             logging.info("load model from {} {}".format(self.parser_args.in_model, parser_args.in_model_index))
@@ -87,7 +75,7 @@ class RosTrainer(object):
             self.global_i_step += 1
             i_step += 1
             infos.append(info)
-            print_step_info(self.train_i_episode, i_step, info)
+            print_step_info(self.train_i_episode, i_step, info, self.train_step_collector)
 
         add_statistics_to_collector(infos=infos, agent_statistics=self.agent.get_statistics(),
                                     episode_info_collector=self.train_collector, env=self.env)
@@ -118,7 +106,8 @@ class RosTrainer(object):
                 self.global_i_step += 1
                 i_step += 1
                 infos.append(info)
-                print_step_info(self.test_i_episode, i_step, info)
+                print_step_info(self.test_i_episode, i_step, info, self.test_step_collector)
+
         add_statistics_to_collector(infos=infos, agent_statistics=self.agent.get_statistics(),
                                     episode_info_collector=self.test_collector, env=self.env)
         add_scalar(self.writer, phase, self.test_collector.get_smooth_statistics(), self.test_i_episode)
@@ -159,3 +148,14 @@ def add_statistics_to_collector(infos: List[Dict], agent_statistics, episode_inf
     if not np.isnan(agent_statistics[0][1]):
         episode_info_collector.add({"average_q": agent_statistics[0][1]})
         episode_info_collector.add({"loss": agent_statistics[1][1]})
+
+
+def print_step_info(episode: int, step: int, info: Dict, step_collector: Dict):
+    for key in info.keys():
+        if key not in step_collector.keys():
+            step_collector[key] = info[key]
+        else:
+            step_collector[key] += info[key]
+    print("Episode : {}; step : {}".format(episode, step))
+    print("Found cells:\n  {}".format(info))
+    print("Accumulated values:\n  {}\n".format(step_collector))