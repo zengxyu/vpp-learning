1231207338da0a861987555be07c48f72e60ed09
zeng <123Zxy>
2022-02-23 20:31:43 +0100

    adjust the weight
    Former-commit-id: ce4389550e1952b505845f35c1d5f9a01e13793e

--- environment/utilities/count_cells_helper.py
+++ environment/utilities/count_cells_helper.py
@@ -24,14 +24,14 @@ def count_observable_cells(env_config, plant_types, plants):
     for plant, type in zip(plants, plant_types):
         observable_roi_total += np.sum(plant == 2) * plant_observable_roi_ratios[type]
         observable_occ_total += np.sum(plant == 1) * plant_observable_occ_ratios[type]
-    return observable_roi_total, observable_occ_total
+    return observable_roi_total + 0.00000001, observable_occ_total + 0.00000001
 
 
 def count_cells(global_map):
     """
     calculate the number of roi cells, occ cells and free cells
     """
-    roi_total = np.sum(global_map == 3)
-    occ_total = np.sum(global_map == 2)
-    free_total = np.sum(global_map == 1)
+    roi_total = np.sum(global_map == 3) + 0.00000001
+    occ_total = np.sum(global_map == 2) + 0.00000001
+    free_total = np.sum(global_map == 1) + 0.00000001
     return roi_total, occ_total, free_total
