129686a3719df41675d46de0ac47d10ccc52069b
zeng <123Zxy>
2022-02-24 19:52:36 +0100

    visualize the observation map
    Former-commit-id: 6bda3a8c964fbe6d6156994154e9f928d1df7b0b

--- visualize/appearance_define.py
+++ visualize/appearance_define.py
@@ -12,10 +12,16 @@
 from matplotlib import pyplot as plt
 from matplotlib.figure import figaspect
 
+colors = ["#ff6666", "#6666ff", "#009999", "#ff9933", "#669900"]
+# rgb_colors = [(255, 102, 102), (102, 102, 255), (0, 153, 153), (256, 153, 51), (102, 153, 0)]
+rgb_colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (102, 153, 0)]
+# rgb_colors = [(255, 50, 50), (50, 255, 50), (50, 50, 255), (255, 255, 0), (102, 153, 0)]
+
 
 def init_plt(i):
     fig = plt.figure(i, figsize=figaspect(1))
     return plt
+
 
 def set_plt_format(plt):
     FONT_SIZE = 14
