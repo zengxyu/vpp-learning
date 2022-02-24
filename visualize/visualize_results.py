b67f469b7e2cd3a78129abdd382e74075dcf3020
zeng <123Zxy>
2022-02-24 03:41:08 +0100

    static env
    Former-commit-id: 24fa91048b7ad67e99c50670ebf476a6cd351c35

--- visualize/visualize_results.py
+++ visualize/visualize_results.py
@@ -20,6 +20,7 @@ from config import read_yaml
 from environment.utilities.count_cells_helper import count_observable_cells
 from environment.utilities.plant_models_loader import load_plants
 from utilities.util import get_project_path
+from visualize.compare_p3d_results_in_multi_env import read_data
 from visualize.plot_format import init_plt, set_plt_format
 
 font_size = 14
@@ -27,34 +28,33 @@ colors = ["#ff6666", "#6666ff", "#009999", "#ff9933", "#669900"]
 font1 = {'size': font_size, }
 
 
-def read_data(data_dir):
-    path = os.path.join(data_dir, "result_log", "ZEvaluation_log.pkl")
-    file = open(path, "rb")
-    env_config = read_yaml(os.path.join(data_dir, "configs"), "env.yaml")
-    plant_models_dir = os.path.join(get_project_path(), "data", 'plant_models')
-
-    plants = load_plants(plant_models_dir, env_config["plant_types"], env_config["roi_neighbors"],
-                         env_config["resolution"])
-    observable_roi_total, observable_occ_total = count_observable_cells(env_config, env_config["plant_types"], plants)
-
-    return pickle.load(file), observable_roi_total, observable_occ_total
+def get_rates_mean_std(occupied_cells_episodes, observable_occ_episodes):
+    occupied_cells_episodes = np.cumsum(occupied_cells_episodes, axis=1)
+    occupied_rates_episodes = []
+    for i, occupied_cells in enumerate(occupied_cells_episodes):
+        occupied_rates_episodes.append(occupied_cells / observable_occ_episodes[i])
+    occupied_rates_episodes = np.array(occupied_rates_episodes)
+    occupied_rates_mean = np.mean(occupied_rates_episodes, axis=0)
+    occupied_rates_std = np.std(occupied_rates_episodes, axis=0)
+    return occupied_rates_mean, occupied_rates_std
 
 
-def plot_coverage_by_time_step(data_dir, data, observable_occ_total):
-    occupied_cells = np.array(data['new_occupied_cells'])
-    rois_cells = np.array(data["new_found_rois"])
-    occupied_rates = np.mean(occupied_cells, axis=0) / observable_occ_total
-    rois_rates = np.mean(rois_cells, axis=0) / observable_roi_total
+def plot_coverage_by_time_step(data_dir, data, observable_roi_totals, observable_occ_totals):
+    rois_cells_episodes = np.array(data["new_found_rois"])
+    occupied_cells_episodes = np.array(data['new_occupied_cells'])
+    rois_rates_mean, rois_rates_std = get_rates_mean_std(rois_cells_episodes, observable_roi_totals)
+    occupied_rates_mean, occupied_rates_std = get_rates_mean_std(occupied_cells_episodes, observable_occ_totals)
 
-    num_time_step = rois_cells.shape[1]
+    num_time_step = rois_cells_episodes.shape[1]
     xs = np.linspace(0, num_time_step, num_time_step)
 
-    ylims = [0.8, 0.7]
-    rates = [occupied_rates, rois_rates]
+    ylims = [1, 1]
+    rates = [occupied_rates_mean, rois_rates_mean]
+    stds = [occupied_rates_std, rois_rates_std]
     y_labels = ["Occupied cell coverage rate", "ROIs coverage rate"]
     names = ["p3d_occ_coverage_rate.png", "p3d_rois_coverage_rate.png"]
-
-    for i, rate in enumerate(rates):
+    i = 0
+    for rate, std in zip(rates, stds):
         fig = plt.figure(i, figsize=figaspect(1))
         FONT_SIZE = 15
 
@@ -68,7 +68,14 @@ def plot_coverage_by_time_step(data_dir, data, observable_occ_total):
 
         plt.grid()
         plt.tight_layout()
-        plt.plot(xs, np.cumsum(rate), colors[i % len(colors)])
+        plt.fill_between(
+            xs,
+            rate - std,
+            rate + std,
+            color="b",
+            alpha=0.1
+        )
+        plt.plot(xs, rate, 'b')
 
         plt.ylabel(y_labels[i])
         plt.xlabel("Time step")
@@ -78,10 +85,11 @@ def plot_coverage_by_time_step(data_dir, data, observable_occ_total):
         plt.savefig(os.path.join(data_dir, "result_log", names[i]))
         plt.clf()
         plt.close()
+        i += 1
 
 
 if __name__ == '__main__':
-    data_dir = "evaluate_action10_sensor300_smallobs_maxsteps400"
+    data_dir = "evaluation/320x320x150_2plants"
     data_dir = os.path.join(get_project_path(), "output", data_dir)
     data, observable_roi_total, observable_occ_total = read_data(data_dir)
-    plot_coverage_by_time_step(data_dir, data, observable_occ_total)
+    plot_coverage_by_time_step(data_dir, data, observable_roi_total, observable_occ_total)
