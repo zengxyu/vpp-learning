d0da0a2f916705e7f87ba92dabbd2ca15d28d6d2
zeng <123Zxy>
2022-02-28 03:21:45 +0100

    create random policy
    Former-commit-id: 5fe684af6cf5609e81a896b6f636ff06918e720b

--- evaluate_random_policy.sh
+++ evaluate_random_policy.sh
@@ -1,23 +1,10 @@
-python3 evaluate_p3d.py "random_policy" --out_folder=evaluate_random_policy/320x320x150_2plants --room_size 320 320 150 --plant_num_choices 2 --num_episodes=30 --max_steps=400 --randomize_sensor_position=0;
-python3 evaluate_p3d.py "random_policy" --out_folder=evaluate_random_policy/400x400x150_2plants --room_size 400 400 150 --plant_num_choices 2 --num_episodes=30 --max_steps=400 --randomize_sensor_position=0;
-python3 evaluate_p3d.py "random_policy" --out_folder=evaluate_random_policy/480x480x150_2plants --room_size 480 480 150 --plant_num_choices 2 --num_episodes=30 --max_steps=400 --randomize_sensor_position=0;
-python3 evaluate_p3d.py "random_policy" --out_folder=evaluate_random_policy/320x480x150_2plants --room_size 320 480 150 --plant_num_choices 2 --num_episodes=30 --max_steps=400 --randomize_sensor_position=0;
-python3 evaluate_p3d.py "random_policy" --out_folder=evaluate_random_policy/400x400x200_2plants --room_size 400 400 200 --plant_num_choices 2 --num_episodes=30 --max_steps=400 --randomize_sensor_position=0;
+python3 evaluate_p3d.py "random_policy" --out_folder=evaluate_random_policy/320x320x150_2plants --room_size 320 320 150 --plant_num_choices 2 --num_episodes=30 --max_steps=400 --random_plant_number=True --randomize_sensor_position=0;
+python3 evaluate_p3d.py "random_policy" --out_folder=evaluate_random_policy/400x400x150_2plants --room_size 400 400 150 --plant_num_choices 2 --num_episodes=30 --max_steps=400 --random_plant_number=True --randomize_sensor_position=0;
+python3 evaluate_p3d.py "random_policy" --out_folder=evaluate_random_policy/480x480x150_2plants --room_size 480 480 150 --plant_num_choices 2 --num_episodes=30 --max_steps=400 --random_plant_number=True --randomize_sensor_position=0;
+python3 evaluate_p3d.py "random_policy" --out_folder=evaluate_random_policy/320x480x150_2plants --room_size 320 480 150 --plant_num_choices 2 --num_episodes=30 --max_steps=400 --random_plant_number=True --randomize_sensor_position=0;
 
-python3 evaluate_p3d.py "random_policy" --out_folder=evaluate_random_policy/320x320x150_4plants --room_size 320 320 150 --plant_num_choices 4 --num_episodes=30 --max_steps=400 --randomize_sensor_position=0;
-python3 evaluate_p3d.py "random_policy" --out_folder=evaluate_random_policy/400x400x150_4plants --room_size 400 400 150 --plant_num_choices 4 --num_episodes=30 --max_steps=400 --randomize_sensor_position=0;
-python3 evaluate_p3d.py "random_policy" --out_folder=evaluate_random_policy/480x480x150_4plants --room_size 480 480 150 --plant_num_choices 4 --num_episodes=30 --max_steps=400 --randomize_sensor_position=0;
-python3 evaluate_p3d.py "random_policy" --out_folder=evaluate_random_policy/320x480x150_4plants --room_size 320 480 150 --plant_num_choices 4 --num_episodes=30 --max_steps=400 --randomize_sensor_position=0;
-python3 evaluate_p3d.py "random_policy" --out_folder=evaluate_random_policy/400x400x200_4plants --room_size 400 400 200 --plant_num_choices 4 --num_episodes=30 --max_steps=400 --randomize_sensor_position=0;
+python3 evaluate_p3d.py "random_policy" --out_folder=evaluate_random_policy/320x320x150_4plants --room_size 320 320 150 --plant_num_choices 4 --num_episodes=30 --max_steps=400 --random_plant_number=True --randomize_sensor_position=0;
+python3 evaluate_p3d.py "random_policy" --out_folder=evaluate_random_policy/400x400x150_4plants --room_size 400 400 150 --plant_num_choices 4 --num_episodes=30 --max_steps=400 --random_plant_number=True --randomize_sensor_position=0;
+python3 evaluate_p3d.py "random_policy" --out_folder=evaluate_random_policy/480x480x150_4plants --room_size 480 480 150 --plant_num_choices 4 --num_episodes=30 --max_steps=400 --random_plant_number=True --randomize_sensor_position=0;
+python3 evaluate_p3d.py "random_policy" --out_folder=evaluate_random_policy/320x480x150_4plants --room_size 320 480 150 --plant_num_choices 4 --num_episodes=30 --max_steps=400 --random_plant_number=True --randomize_sensor_position=0;
 
-python3 evaluate_p3d.py "random_policy" --out_folder=evaluate_random_policy/320x320x150_6plants --room_size 320 320 150 --plant_num_choices 6 --num_episodes=30 --max_steps=400 --randomize_sensor_position=0;
-python3 evaluate_p3d.py "random_policy" --out_folder=evaluate_random_policy/400x400x150_6plants --room_size 400 400 150 --plant_num_choices 6 --num_episodes=30 --max_steps=400 --randomize_sensor_position=0;
-python3 evaluate_p3d.py "random_policy" --out_folder=evaluate_random_policy/480x480x150_6plants --room_size 480 480 150 --plant_num_choices 6 --num_episodes=30 --max_steps=400 --randomize_sensor_position=0;
-python3 evaluate_p3d.py "random_policy" --out_folder=evaluate_random_policy/320x480x150_6plants --room_size 320 480 150 --plant_num_choices 6 --num_episodes=30 --max_steps=400 --randomize_sensor_position=0;
-python3 evaluate_p3d.py "random_policy" --out_folder=evaluate_random_policy/400x400x200_6plants --room_size 400 400 200 --plant_num_choices 6 --num_episodes=30 --max_steps=400 --randomize_sensor_position=0;
-
-python3 evaluate_p3d.py "random_policy" --out_folder=evaluate_random_policy/320x320x150_8plants --room_size 320 320 150 --plant_num_choices 8 --num_episodes=30 --max_steps=400 --randomize_sensor_position=0;
-python3 evaluate_p3d.py "random_policy" --out_folder=evaluate_random_policy/400x400x150_8plants --room_size 400 400 150 --plant_num_choices 8 --num_episodes=30 --max_steps=400 --randomize_sensor_position=0;
-python3 evaluate_p3d.py "random_policy" --out_folder=evaluate_random_policy/480x480x150_8plants --room_size 480 480 150 --plant_num_choices 8 --num_episodes=30 --max_steps=400 --randomize_sensor_position=0;
-python3 evaluate_p3d.py "random_policy" --out_folder=evaluate_random_policy/320x480x150_8plants --room_size 320 480 150 --plant_num_choices 8 --num_episodes=30 --max_steps=400 --randomize_sensor_position=0;
-python3 evaluate_p3d.py "random_policy" --out_folder=evaluate_random_policy/400x400x200_8plants --room_size 400 400 200 --plant_num_choices 8 --num_episodes=30 --max_steps=400 --randomize_sensor_position=0;
