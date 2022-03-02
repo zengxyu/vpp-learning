3e25f2afd3e03b3b480babde2996fa60a9797761
zeng <123Zxy>
2022-03-02 01:31:48 +0100

    change the name of sh files

--- evaluate_circular_policy.sh
+++
@@ -1,5 +0,0 @@
-python3 evaluate_p3d.py "circular_policy" --out_folder=evaluate_circular_policy/320x320x150_2plants --room_size 320 320 150 --plant_num_choices 2 --num_episodes=30 --max_steps=400 --random_plant_number=True --randomize_sensor_position=0;
-python3 evaluate_p3d.py "circular_policy" --out_folder=evaluate_circular_policy/400x400x150_2plants --room_size 400 400 150 --plant_num_choices 2 --num_episodes=30 --max_steps=400 --random_plant_number=True --randomize_sensor_position=0;
-python3 evaluate_p3d.py "circular_policy" --out_folder=evaluate_circular_policy/480x480x150_2plants --room_size 480 480 150 --plant_num_choices 2 --num_episodes=30 --max_steps=400 --random_plant_number=True --randomize_sensor_position=0;
-python3 evaluate_p3d.py "circular_policy" --out_folder=evaluate_circular_policy/320x480x150_2plants --room_size 320 480 150 --plant_num_choices 2 --num_episodes=30 --max_steps=400 --random_plant_number=True --randomize_sensor_position=0;
-
--- evaluate_random_policy2.sh
+++
@@ -1,9 +0,0 @@
-python3 evaluate_p3d.py "random_policy" --out_folder=evaluate_random_policy/320x320x150_6plants --room_size 320 320 150 --plant_num_choices 6 --num_episodes=30 --max_steps=400 --random_plant_number=True --randomize_sensor_position=0;
-python3 evaluate_p3d.py "random_policy" --out_folder=evaluate_random_policy/400x400x150_6plants --room_size 400 400 150 --plant_num_choices 6 --num_episodes=30 --max_steps=400 --random_plant_number=True --randomize_sensor_position=0;
-python3 evaluate_p3d.py "random_policy" --out_folder=evaluate_random_policy/480x480x150_6plants --room_size 480 480 150 --plant_num_choices 6 --num_episodes=30 --max_steps=400 --random_plant_number=True --randomize_sensor_position=0;
-python3 evaluate_p3d.py "random_policy" --out_folder=evaluate_random_policy/320x480x150_6plants --room_size 320 480 150 --plant_num_choices 6 --num_episodes=30 --max_steps=400 --random_plant_number=True --randomize_sensor_position=0;
-
-python3 evaluate_p3d.py "random_policy" --out_folder=evaluate_random_policy/320x320x150_8plants --room_size 320 320 150 --plant_num_choices 8 --num_episodes=30 --max_steps=400 --random_plant_number=True --randomize_sensor_position=0;
-python3 evaluate_p3d.py "random_policy" --out_folder=evaluate_random_policy/400x400x150_8plants --room_size 400 400 150 --plant_num_choices 8 --num_episodes=30 --max_steps=400 --random_plant_number=True --randomize_sensor_position=0;
-python3 evaluate_p3d.py "random_policy" --out_folder=evaluate_random_policy/480x480x150_8plants --room_size 480 480 150 --plant_num_choices 8 --num_episodes=30 --max_steps=400 --random_plant_number=True --randomize_sensor_position=0;
-python3 evaluate_p3d.py "random_policy" --out_folder=evaluate_random_policy/320x480x150_8plants --room_size 320 480 150 --plant_num_choices 8 --num_episodes=30 --max_steps=400 --random_plant_number=True --randomize_sensor_position=0;
